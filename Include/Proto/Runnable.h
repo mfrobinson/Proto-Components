683a517286e38c8b537825d3323eae3c5b4d6089
mfrobinson <mrobinson385a@gmail.com>
2026-03-25 19:31:58 +0000

    Add project files.

---
+++ Include/Proto/Runnable.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <thread>
+#include <mutex>
+#include <memory>
+
+
+namespace proto {
+
+	class Runnable {
+	public:
+		Runnable();
+		Runnable(Runnable&& other);
+		Runnable(const Runnable& other) = delete;
+		Runnable& operator=(Runnable&& other);
+		Runnable& operator=(const Runnable& other) = delete;
+		~Runnable();
+
+		void run();
+		void start();
+		void stop();
+		bool running();
+	protected:
+		virtual void execute() = 0;
+		bool should_run();
+	private:
+		bool keep_running;
+		bool done_running;
+		std::unique_ptr<std::mutex> state_mutex_ptr;
+		std::unique_ptr<std::condition_variable> run_complete_condition_ptr;
+
+		inline void move_from(Runnable& other);
+		inline void copy_from(const Runnable& other) = delete;
+		inline void cleanup();
+	};
+
+}
---
+++ Src/Runnable.cpp
@@ -0,0 +1,84 @@
+#include "Proto/Runnable.h"
+
+
+namespace proto {
+
+	Runnable::Runnable() : keep_running(false), done_running(true) {
+		return;
+	}
+	Runnable::Runnable(Runnable&& other) {
+		this->move_from(other);
+		return;
+	}
+	Runnable& Runnable::operator=(Runnable&& other) {
+		if (&other != this) {
+			this->cleanup();
+			this->move_from(other);
+		}
+		return *this;
+	}
+	Runnable::~Runnable() {
+		this->cleanup();
+		return;
+	}
+
+	void Runnable::run() {
+		if (this->running()) {
+			return;
+		}
+
+		std::unique_lock<std::mutex> state_lock(*this->state_mutex_ptr);
+		this->keep_running = true;
+		this->done_running = false;
+		state_lock.unlock();
+
+		this->execute();
+
+		state_lock.lock();
+		this->keep_running = true;
+		this->done_running = false;
+		state_lock.unlock();
+
+		return;
+	}
+	void Runnable::start() {
+		std::thread runner(&Runnable::run, this);
+		runner.detach();
+		return;
+	}
+	void Runnable::stop() {
+		std::unique_lock<std::mutex> state_lock(*this->state_mutex_ptr);
+		if (this->keep_running == false || this->done_running == true) {
+			state_lock.unlock();
+			return;
+		}
+		this->keep_running = false;
+		state_lock.unlock();
+		this->run_complete_condition_ptr->wait(state_lock, [this]() {
+			return this->done_running;
+		});
+		return;
+	}
+	bool Runnable::running() {
+		std::lock_guard<std::mutex> state_guard(*this->state_mutex_ptr);
+		return this->keep_running || !this->done_running;
+	}
+
+
+	inline void Runnable::move_from(Runnable& other) {
+		this->keep_running = other.keep_running;
+		this->done_running = other.done_running;
+		this->state_mutex_ptr = std::move(other.state_mutex_ptr);
+		this->run_complete_condition_ptr = std::move(other.run_complete_condition_ptr);
+		other.keep_running = false;
+		other.done_running = true;
+		other.state_mutex_ptr = std::make_unique<std::mutex>();
+		other.run_complete_condition_ptr = std::make_unique<std::condition_variable>();
+		return;
+	}
+	inline void Runnable::cleanup() {
+		this->stop();
+		return;
+	}
+
+}
