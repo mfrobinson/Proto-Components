2800cd06290639674e2eac4f3851029b742e919d
mfrobinson <mrobinson385a@gmail.com>
2026-05-25 06:01:40 +0100

    Added CompletionPortRunnable for Windows

---
+++ Include/Proto/Components/Windows/CompletionPortRunnable.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Proto/Components/Runnable.h"
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+
+namespace proto::components {
+
+	class CompletionPortRunnable : public Runnable {
+	public:
+		CompletionPortRunnable();
+		~CompletionPortRunnable();
+	protected:
+		HANDLE completion_port;
+
+		virtual bool on_start() override;
+		virtual void interrupt() override;
+		virtual void execute() override;
+
+		virtual void handle_completion_packet(DWORD bytes_transferred, ULONG_PTR completion_key, LPOVERLAPPED overlapped) = 0;
+		virtual void cleanup_completion_key(ULONG_PTR completion_key) = 0;
+		virtual void cleanup_overlapped(LPOVERLAPPED overlapped) = 0;
+	private:
+	};
+
+}
---
+++ Src/Proto/Components/Windows/CompletionPortRunnable.cpp
@@ -0,0 +1,43 @@
+#include "Proto/Components/Windows/CompletionPortRunnable.h"
+
+
+namespace proto::components {
+
+	CompletionPortRunnable::CompletionPortRunnable() {
+		return;
+	}
+
+	CompletionPortRunnable::~CompletionPortRunnable() {
+		if (this->completion_port != NULL) CloseHandle(this->completion_port);
+		return;
+	}
+
+	bool CompletionPortRunnable::on_start() {
+		if (this->completion_port == NULL) this->completion_port = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 0);
+		if (this->completion_port == NULL) return false;
+		return true;
+	}
+
+	void CompletionPortRunnable::interrupt() {
+		PostQueuedCompletionStatus(this->completion_port, 0, NULL, NULL);
+		return;
+	}
+
+	void CompletionPortRunnable::execute() {
+		DWORD bytes_transferred = 0;
+		ULONG_PTR completion_key = NULL;
+		LPOVERLAPPED overlapped = NULL;
+
+		bool result = GetQueuedCompletionStatus(this->completion_port, &bytes_transferred, &completion_key, &overlapped, INFINITE);
+		while (result && overlapped != NULL) {
+			this->handle_completion_packet(bytes_transferred, completion_key, overlapped);
+			if (overlapped != NULL) this->cleanup_overlapped(overlapped);
+			result = GetQueuedCompletionStatus(this->completion_port, &bytes_transferred, &completion_key, &overlapped, INFINITE);
+		}
+
+		if (completion_key != NULL) this->cleanup_completion_key(completion_key);
+		if (overlapped != NULL) this->cleanup_overlapped(overlapped);
+		return;
+	}
+
+}
