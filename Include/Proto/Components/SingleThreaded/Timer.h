413564876441c5982b55c05c9085db175b931312
mfrobinson <mrobinson385a@gmail.com>
2026-06-28 18:04:09 +0100

    Added functionality to cancel timeouts in the Timer class

--- Include/Proto/Components/SingleThreaded/Timer.h
+++ Include/Proto/Components/SingleThreaded/Timer.h
@@ -6,6 +6,7 @@
 #include <queue>
 #include <vector>
 #include <condition_variable>
+#include <set>
 
 
 namespace proto::components::single_threaded {
@@ -13,30 +14,34 @@ namespace proto::components::single_threaded {
 	class Timer : public Runnable {
 	public:
 		Timer();
-		Timer(Timer&& other);
+		Timer(Timer&& other) = delete;
 		Timer(const Timer& other) = delete;
-		Timer& operator=(Timer&& other);
+		Timer& operator=(Timer&& other) = delete;
 		Timer& operator=(const Timer& other) = delete;
 		~Timer();
 
-		void set_timeout(const std::chrono::nanoseconds timeout, std::function<void()> callback);
+		uint64_t set_timeout(const std::chrono::nanoseconds timeout, std::function<void()> callback);
+		void cancel_timeout(const uint64_t timeout_id);
 	protected:
 		virtual bool on_start() override;
 		virtual void execute() override;
 		virtual void interrupt() override;
 	private:
 		struct TimeoutInfo {
+			uint64_t timeout_id;
 			std::chrono::steady_clock::time_point timeout_point;
 			std::function<void()> callback;
 		};
 		struct GreaterTimeoutInfo {
-			bool operator()(const TimeoutInfo& t1, const TimeoutInfo& t2) {
-				return t1.timeout_point > t2.timeout_point;
+			static bool operator()(const TimeoutInfo& t1, const TimeoutInfo& t2) {
+				return t1.timeout_point < t2.timeout_point;
 			}
 		};
+		uint64_t next_timeout_id;
 		std::mutex queue_mutex;
 		// Min heap to order the timeouts
-		std::priority_queue<TimeoutInfo, std::vector<TimeoutInfo>, GreaterTimeoutInfo> timeout_queue;
+		std::unordered_map<uint64_t, std::multiset<TimeoutInfo, GreaterTimeoutInfo>::iterator> iterators_of_ids;
+		std::multiset<TimeoutInfo, GreaterTimeoutInfo> timeout_queue;
 
 		std::condition_variable_any timeout_cv;
 		std::stop_source stop_source;
--- Src/Proto/Components/SingleThreaded/Timer.cpp
+++ Src/Proto/Components/SingleThreaded/Timer.cpp
@@ -3,7 +3,7 @@
 
 namespace proto::components::single_threaded {
 
-	Timer::Timer() : Runnable() {
+	Timer::Timer() : Runnable(), next_timeout_id(0) {
 		return;
 	}
 
@@ -12,17 +12,32 @@ namespace proto::components::single_threaded {
 		return;
 	}
 
-	void Timer::set_timeout(const std::chrono::nanoseconds timeout, std::function<void()> callback) {
+	uint64_t Timer::set_timeout(const std::chrono::nanoseconds timeout, std::function<void()> callback) {
 		auto target_time = std::chrono::steady_clock::now() + timeout;
+		uint64_t timeout_id;
 		{
 			std::lock_guard<std::mutex> lock(this->queue_mutex);
-			this->timeout_queue.push({ target_time, std::move(callback) });
+			timeout_id = this->next_timeout_id++;
+			auto iterator = this->timeout_queue.emplace(timeout_id, target_time, std::move(callback));
+			this->iterators_of_ids[timeout_id] = iterator;
+
 		}
 		this->timeout_cv.notify_all();
+		return timeout_id;
+	}
+
+	void Timer::cancel_timeout(const uint64_t timeout_id) {
+		std::lock_guard<std::mutex> lock(this->queue_mutex);
+		auto iterator = this->iterators_of_ids.find(timeout_id);
+		if (iterator != this->iterators_of_ids.end()) {
+			this->timeout_queue.erase(iterator->second);
+			this->iterators_of_ids.erase(iterator);
+		}
+		return;
 	}
 
 	bool Timer::on_start() {
-		this->timeout_queue = std::priority_queue<TimeoutInfo, std::vector<TimeoutInfo>, GreaterTimeoutInfo>();
+		this->timeout_queue = std::multiset<TimeoutInfo, GreaterTimeoutInfo>();
 		this->stop_source = std::stop_source();
 		return true;
 	}
@@ -37,12 +52,12 @@ namespace proto::components::single_threaded {
 			}
 
 			auto now = std::chrono::steady_clock::now();
-			auto& next_timeout = this->timeout_queue.top();
+			auto iterator_of_next_timeout = this->timeout_queue.begin();
 
-			if (next_timeout.timeout_point <= now) {
-				TimeoutInfo current = std::move(const_cast<TimeoutInfo&>(next_timeout));
-				this->timeout_queue.pop();
-
+			if (iterator_of_next_timeout->timeout_point <= now) {
+				TimeoutInfo current = std::move(*iterator_of_next_timeout);
+				this->timeout_queue.erase(iterator_of_next_timeout);
+				this->iterators_of_ids.erase(current.timeout_id);
 				queue_lock.unlock();
 				current.callback();
 				queue_lock.lock();
@@ -50,7 +65,7 @@ namespace proto::components::single_threaded {
 			else {
 				size_t number_of_timeouts_before_wait = this->timeout_queue.size();
 				// Wait until the timer is stopped, the next timeout in order is reached, or a NEW timeout is added (which could be sooner than the previous next_timeout)
-				this->timeout_cv.wait_until(queue_lock, this->stop_source.get_token(), next_timeout.timeout_point, [this, number_of_timeouts_before_wait] {return this->timeout_queue.size() > number_of_timeouts_before_wait; });
+				this->timeout_cv.wait_until(queue_lock, this->stop_source.get_token(), iterator_of_next_timeout->timeout_point, [this, number_of_timeouts_before_wait] {return this->timeout_queue.size() > number_of_timeouts_before_wait; });
 			}
 		}
 		return;
